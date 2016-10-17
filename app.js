43eb13f43477f8d830716d039720435e5963c7c2
yoona <09292@skcc.com>
2016-10-17 10:51:33 +0900

    express

--- app.js
+++ app.js
@@ -1,30 +1,10 @@
-var http = require('http');
-var os = require('os');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-  console.log(`-> ${request.url} ${request.method}`);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-  switch (request.url) {
-    case '/cpu':
-              var cpus = JSON.stringify(os.cpus()[0].model);
-              response.end(cpus);
-              break;
-    default:
-              response.end('not found');
-              break;
-  }
-  //
-  // request.on('error', function(err) {
-  //   console.error(err);
-  // }).on('data', function(chunk) {
-  //   body.push(chunk);
-  // }).on('end', function() {
-  //   body = Buffer.concat(body).toString();
-  //   // 여기서 헤더, 메소드, url, 바디를 가지게 되었고
-  //   // 이 요청에 응답하는 데 필요한 어떤 일이라도 할 수 있게 되었습니다.
-  // });
-}).listen(8080); // 이 서버를 활성화하고 8080 포트로 받습니다.
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
